f08cbd672f3769a15f8533c0ae2672d8490f571b
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-18 17:04:20 -0300

    faker factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,24 +4,23 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make(env('DEFAULT_USER_PASSWORD', '12345678')),
             'remember_token' => Str::random(10),
         ];
     }
