0370898427e9a96ad5748dd993dbf9a9bc83af46
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-21 11:24:40 -0300

    Create ListCategories.php

---
+++ app/Console/Commands/ListCategories.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class ListCategories extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:list-categories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Lists a chunk of available categories, starting randomly.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle() {
+        $category_count = Redis::get('category_count');
+        $rnd = rand(1, $category_count);
+        $response = Http::get(env('LIBRARY_API_URL') . '/api/categories?start=' . $rnd);
+        if ($response->status() !== 200) {
+            Misc::monitor($this->signature, 'Failed.', $response->status());
+            $this->error('Fetch failed with status code: ' . $response->status() . '.');
+            return -1;
+        }
+        $message = 'Listed ' . count($response->json()['data']) . ' categories.';
+        Misc::monitor($this->signature, $message, $response->status());
+        $this->info($message);
+        return 0;
+    }
+}
