0631b0224a22aaf5d6bb590c13b19a8a3df12575
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-24 08:34:50 -0300

    referer safe tests

---
+++ tests/Feature/RefererSafeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Services\Misc;
+
+class RefererSafeTest extends TestCase {
+    public function test_some_referer_strings(): void {
+        $this->assertFalse(Misc::isRefererSafe(null));
+        $this->assertFalse(Misc::isRefererSafe(''));
+        $this->assertFalse(Misc::isRefererSafe('http://local'));
+        $this->assertTrue(Misc::isRefererSafe(env('APP_URL')));
+        $this->assertTrue(Misc::isRefererSafe(env('APP_URL') . ':8000'));
+        $this->assertTrue(Misc::isRefererSafe(env('APP_URL') . '/'));
+        $this->assertTrue(Misc::isRefererSafe(env('APP_URL') . '/test'));
+        $this->assertFalse(Misc::isRefererSafe(env('APP_URL') . 'abcd'));
+        $this->assertTrue(Misc::isRefererSafe(env('APP_URL') . '/api/monitor'));
+        $this->assertFalse(Misc::isRefererSafe(env('LIBRARY_API_URL') . '/api/books/1'));
+    }
+}
